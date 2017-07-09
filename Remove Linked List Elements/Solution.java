e791746546175b4e2a18fd3bdcf1c343bd43e595
dpronin <dpronin@yandex.ru>
2017-07-09 16:04:07 -0700

    Create Solution.java

---
+++ Remove Linked List Elements/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+        
+    }
+}
