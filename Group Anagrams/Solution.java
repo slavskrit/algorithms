03cbf68464ceda095f71b83e64d61d6d3acb5d2a
dpronin <dpronin@yandex.ru>
2017-07-09 14:45:11 -0700

    Create Solution.java

---
+++ Group Anagrams/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    private static String badHash(String string) {
+        char[] result = new char[26];
+        int[] hash = new int[26];
+        if (string.length() == 0) return "";
+        for (int i = 0; i < string.length(); i++) {
+            hash[string.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] == 0) {
+                result[i] = ' ';
+            } else {
+                result[i] = (char) hash[i];
+            }
+        }
+        return new String(result);
+    }
+
+     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String string: strs) {
+            String t = badHash(string);
+            List<String> temp = map.get(t);
+            if (temp == null) {
+                temp = new ArrayList<>();
+                temp.add(string);
+                map.put(t, temp);
+            } else {
+                temp.add(string);
+            }
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
