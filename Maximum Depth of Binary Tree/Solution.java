d08143fa9335a156eeedb5d76aa29a3d71ad812d
dpronin <dpronin@yandex.ru>
2017-07-09 15:18:44 -0700

    Create Solution.java

---
+++ Maximum Depth of Binary Tree/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return (left > right) ? left + 1 : right + 1;
+
+    }
+}
