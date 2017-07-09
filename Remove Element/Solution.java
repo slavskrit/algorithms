705b99390596b6d5a9c5ac577266477ce780e767
dpronin <dpronin@yandex.ru>
2017-07-09 14:37:51 -0700

    Create Solution.java

---
+++ Remove Element/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+}
