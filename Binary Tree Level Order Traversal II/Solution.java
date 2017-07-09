5a65063a505e25486ecfb3b317811bdc4c5659a4
dpronin <dpronin@yandex.ru>
2017-07-09 15:19:18 -0700

    Create Solution.java

---
+++ Binary Tree Level Order Traversal II/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode t = queue.poll();
+                temp.add(t.val);
+                if (t.left != null) queue.add(t.left);
+                if (t.right != null) queue.add(t.right);
+            }
+            result.add(temp);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
