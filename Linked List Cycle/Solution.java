b6e09286057b125ed7af37f8fe10ee54039b4a99
dpronin <dpronin@yandex.ru>
2017-07-09 15:35:10 -0700

    Create Solution.java

---
+++ Linked List Cycle/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        if (head.next == null) return false;
+        if (head.next.next == null) return false;
+        ListNode cursor1 = head;
+        ListNode cursor2 = head.next.next;
+        while(cursor2.next != null && cursor2.next.next != null) {
+            cursor1 = cursor1.next;
+            cursor2 = cursor2.next.next;
+            if(cursor1 == cursor2) return true;
+        }
+        return false;
+    }
+}
