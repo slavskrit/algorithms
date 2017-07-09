c4fc5db9a39717c0ea84fc8f5f32cdeead664f75
dpronin <dpronin@yandex.ru>
2017-07-09 15:49:50 -0700

    Create Solution.java

---
+++ Two Sum II - Input array is sorted/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while(numbers[left] + numbers[right] != target) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        result[0] = ++left;
+        result[1] = ++right;
+        return result;
+    }
+}
