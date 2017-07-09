5ad09f3c94735ca0a0eace2db0118131980990b0
dpronin <dpronin@yandex.ru>
2017-07-09 16:46:47 -0700

    Create Solution.java

---
+++ Battleships in a Board/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int countBattleships(char[][] board) {
+        int result = 0;
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[0].length; x++) {
+                if (board[y][x] == '.') continue;
+                if (x > 0 && board[y][x - 1] == 'X') continue;
+                if (y > 0 && board[y -1][x] == 'X') continue;
+                result++;
+            }
+        }
+        return result;
+    }
+}
