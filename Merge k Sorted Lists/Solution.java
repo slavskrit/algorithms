3eb146491cf90c8ce2add6de6a57eb1283cc16ab
dpronin <dpronin@yandex.ru>
2017-07-09 14:33:55 -0700

    Create Solution.java

---
+++ Merge k Sorted Lists/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((o1, o2) -> o1.val - o2.val);
+        for (ListNode node : lists) {
+            if (node != null) heap.offer(node);
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cursor = dummy;
+        while (!heap.isEmpty()) {
+            ListNode temp = heap.poll();
+            cursor.next = temp;
+            if (temp.next != null) heap.offer(temp.next);
+            temp.next = null;
+            cursor = cursor.next;
+        }
+        return dummy.next;
+    }
+}
