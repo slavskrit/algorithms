7826c4c04e656e3ac0315b93e4bc8f4350a197c7
dpronin <dpronin@yandex.ru>
2017-07-09 16:11:07 -0700

    Create Solution.java

---
+++ Invert Binary Tree/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode t = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(t);
+        return root;
+    }
+}
