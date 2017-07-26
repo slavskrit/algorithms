1ef06b347e5e9693feca66eda0910ab37419e018
dpronin <dpronin@yandex.ru>
2017-07-25 17:04:19 -0700

    Update Solution.java

--- Battleships in a Board/Solution.java
+++ Battleships in a Board/Solution.java
@@ -3,9 +3,9 @@ public class Solution {
         int result = 0;
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[0].length; x++) {
-                if (board[y][x] == '.') continue;
-                if (x > 0 && board[y][x - 1] == 'X') continue;
-                if (y > 0 && board[y -1][x] == 'X') continue;
+                if (board[y][x] == '.' 
+                    || x > 0 && board[y][x - 1] == 'X' 
+                    || y > 0 && board[y -1][x] == 'X' ) continue;
                 result++;
             }
         }
