73e41372fa8bb91fe8db0f6cfe3ec6546c21819e
dpronin <dpronin@yandex.ru>
2017-08-03 16:33:42 -0700

    Create Solution.java

---
+++ Container With Most Water/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int result = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            result = Math.max(result, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+}
