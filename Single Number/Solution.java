e8a34e948444adc648ff58f40580df1705883565
dpronin <dpronin@yandex.ru>
2017-08-02 18:22:32 -0700

    Update Solution.java

--- Single Number/Solution.java
+++ Single Number/Solution.java
@@ -1,9 +1,7 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        int result = nums[0];
-        for (int i = 1; i < nums.length; i ++) {
-            result ^= nums[i] ;
-        }  
+        int result = 0;
+        for (int i : nums) result ^= i;
         return result;
     }
 }
