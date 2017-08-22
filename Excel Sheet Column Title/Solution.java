4ca9a60aa4b5c88d6afaba2d0c062bc8e8f73425
dpronin <dpronin@yandex.ru>
2017-08-22 11:12:28 -0700

    Update Solution.java

--- Excel Sheet Column Title/Solution.java
+++ Excel Sheet Column Title/Solution.java
@@ -1,12 +1,11 @@
 public class Solution {
     public String convertToTitle(int n) {
+        if (n == 0) return "";
         StringBuilder result = new StringBuilder();
-        if (n == 0) return result.toString();
-        while (n > 0) {
-            n--;
-            result.append((char) (n % 26 + 'A'));
+        while (n-- > 0) {
+            result.insert(0, (char) (n % 26 + 'A'));
             n /= 26;
         }
-        return result.reverse().toString();
+        return result.toString();
     }
 }
