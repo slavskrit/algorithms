43d78446c945a404f7cc167330de1ef1c105e57b
dpronin <dpronin@yandex.ru>
2017-08-22 09:33:58 -0700

    Update Solution.java

--- Sqrt-x/Solution.java
+++ Sqrt-x/Solution.java
@@ -1,7 +1,8 @@
 public class Solution {
     public int mySqrt(int x) {
         if (x == 0) return 0;
-        for (int l = 1, r = x ;;){
+        int l = 1, r = x;
+        while (true) {
             int mid = l + (r - l) / 2;
             if (mid > x / mid) {
                 r = mid - 1;
