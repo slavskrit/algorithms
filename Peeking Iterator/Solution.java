c10e31d3c6bc252de9cc241049f3b8624d6e2b89
dpronin <dpronin@yandex.ru>
2017-08-22 10:48:14 -0700

    Update Solution.java

--- Peeking Iterator/Solution.java
+++ Peeking Iterator/Solution.java
@@ -5,28 +5,28 @@ class PeekingIterator implements Iterator<Integer> {
     private Integer next;
     private Iterator<Integer> iterator;
     
-	public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
-	    this.iterator = iterator;
+    public PeekingIterator(Iterator<Integer> iterator) {
+	// initialize any member here.
+        this.iterator = iterator;
         if (iterator.hasNext()) next = this.iterator.next();
-	}
+    }
 
     // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
+    public Integer peek() {
         return next;
-	}
+    }
 
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-	    Integer temp = next;
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer temp = next;
         next = iterator.hasNext() ? iterator.next() : null;
         return temp;
-	}
+    }
 
-	@Override
-	public boolean hasNext() {
-	    return next != null;
-	}
+    @Override
+    public boolean hasNext() {
+	return next != null;
+    }
 }
