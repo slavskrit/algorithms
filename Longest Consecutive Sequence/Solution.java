5fa1619ea12e34f7e64ff8377833d802fd9d4930
dpronin <dpronin@yandex.ru>
2017-08-24 15:11:57 -0700

    Update Solution.java

--- Longest Consecutive Sequence/Solution.java
+++ Longest Consecutive Sequence/Solution.java
@@ -2,16 +2,16 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
         int result = 1;
-        Set<Integer> set = new HashSet<>();
-        for (int i : nums) set.add(i);
-        for (int i : set) {
-            if (!set.contains(i - 1)) {
-                int search = i + 1;
-                while (set.contains(search)) {
-                    search++;
-                }
-                result = Math.max(result, search - i);
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (map.containsKey(i)) continue;
+            int l = map.getOrDefault(i - 1, 0);
+            int r = map.getOrDefault(i + 1, 0);
+            int temp = l + r + 1; // at least 1 consecutive digit is current digit itself
+            map.put(i, temp);
+            map.put(i - l, temp);
+            map.put(i + r, temp);
+            result = Math.max(result, temp);
         }
         return result;
     }
