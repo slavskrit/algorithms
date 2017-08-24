2a7c456006046e7db8cc544283e243f36f476eb7
dpronin <dpronin@yandex.ru>
2017-08-24 09:16:04 -0700

    Update Solution.java

--- Judge Route Circle/Solution.java
+++ Judge Route Circle/Solution.java
@@ -2,10 +2,10 @@ public class Solution {
     public boolean judgeCircle(String moves) {
         int v = 0, h = 0;
         for (char c : moves.toCharArray()) {
-            if(c == 'U') v++;
-            else if(c == 'D') v--;
-            else if(c == 'R') h++;
-            else if(c == 'L') h--;
+            if (c == 'U') v++;
+            else if (c == 'D') v--;
+            else if (c == 'R') h++;
+            else if (c == 'L') h--;
         }
         return v == h && h == 0;
     }
