82f28e701875b137fd616bfd5b58c24a991253d4
dpronin <dpronin@yandex.ru>
2017-09-10 21:26:52 -0700

    Update solution.py

--- Flatten Binary Tree to Linked List/solution.py
+++ Flatten Binary Tree to Linked List/solution.py
@@ -1,38 +1,38 @@
-// https://leetcode.com/problems/flatten-binary-tree-to-linked-list
-// 
-// 
-// Given a binary tree, flatten it to a linked list in-place.
-// 
-// 
-// For example,
-// Given
-// 
-//          1
-//         / \
-//        2   5
-//       / \   \
-//      3   4   6
-// 
-// 
-// 
-// The flattened tree should look like:
-// 
-//    1
-//     \
-//      2
-//       \
-//        3
-//         \
-//          4
-//           \
-//            5
-//             \
-//              6
-// 
-// click to show hints.
-// Hints:
-// If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
-//
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+#  
+#  
+# Given a binary tree, flatten it to a linked list in-place.
+#  
+#  
+#  For example,
+#  Given
+#  
+#           1
+#          / \
+#         2   5
+#        / \   \
+#       3   4   6
+#  
+#  
+#  
+#  The flattened tree should look like:
+#  
+#     1
+#      \
+#       2
+#        \
+#         3
+#          \
+#           4
+#            \
+#             5
+#              \
+#               6
+#  
+#  click to show hints.
+#  Hints:
+#  If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+# 
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
