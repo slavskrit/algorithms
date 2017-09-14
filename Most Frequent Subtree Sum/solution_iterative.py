f394ab5dfbf7b20cd0b10eba475ac9edfac6f044
dpronin <dpronin@yandex.ru>
2017-09-14 15:29:58 -0700

    Update solution_iterative.py

--- Most Frequent Subtree Sum/solution_iterative.py
+++ Most Frequent Subtree Sum/solution_iterative.py
@@ -1,4 +1,4 @@
-```
+'''
 https://leetcode.com/problems/most-frequent-subtree-sum
 
 Given the root of a tree, you are asked to find the most frequent subtree sum. The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself). So what is the most frequent subtree sum value? If there is a tie, return all the values with the highest frequency in any order.
@@ -18,7 +18,7 @@ Input:
 2   -5
 return [2], since 2 happens twice, however -5 only occur once.
 Note: You may assume the sum of values in any subtree is in the range of 32-bit signed integer.
-```
+'''
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
