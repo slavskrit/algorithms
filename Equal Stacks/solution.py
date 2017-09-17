7990345f2d73dd680d8ca035f04db21af3d55a72
dpronin <dpronin@yandex.ru>
2017-09-16 17:46:08 -0700

    Create solution.py

---
+++ Equal Stacks/solution.py
@@ -0,0 +1,45 @@
+"""
+https://www.hackerrank.com/challenges/equal-stacks/problem
+
+You have three stacks of cylinders where each cylinder has the same diameter, but they may vary in height. You can change the height of a stack by removing and discarding its topmost cylinder any number of times.
+
+Find the maximum possible height of the stacks such that all of the stacks are exactly the same height. This means you must remove zero or more cylinders from the top of zero or more of the three stacks until they're all the same height, then print the height. The removals must be performed in such a way as to maximize the height.
+
+Note: An empty stack is still a stack.
+
+Input Format
+
+The first line contains three space-separated integers, , , and , describing the respective number of cylinders in stacks , , and . The subsequent lines describe the respective heights of each cylinder in a stack from top to bottom:
+
+The second line contains  space-separated integers describing the cylinder heights in stack .
+The third line contains  space-separated integers describing the cylinder heights in stack .
+The fourth line contains  space-separated integers describing the cylinder heights in stack .
+Constraints
+
+Output Format
+
+Print a single integer denoting the maximum height at which all stacks will be of equal height.
+
+Sample Input
+
+5 3 4
+3 2 1 1 1
+4 3 2
+1 1 4 1
+Sample Output
+
+5
+"""
+#!/bin/python3
+
+import sys
+
+stacks_len = len(input().split())  # skip first line since it's useless
+stacks = [list(map(int, input().strip().split())) for _ in range(stacks_len)]
+heights = [sum(i) for i in stacks]
+
+while max(heights) != min(heights):
+    _ = heights.index(max(heights))
+    heights[_] -= stacks[_].pop(0)
+
+print(heights[0])
