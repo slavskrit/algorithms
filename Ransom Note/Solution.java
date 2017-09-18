d07383df30959a2133fcab791ac800cf3da8f413
dpronin <dpronin@yandex.ru>
2017-09-18 16:15:14 -0700

    Update Solution.java

--- Ransom Note/Solution.java
+++ Ransom Note/Solution.java
@@ -18,19 +18,10 @@
 // 
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int chars[] = new int[26];
-        char r[] = ransomNote.toCharArray();
-        char m[] = magazine.toCharArray();
-        for (int i = 0; i < r.length; i++) {
-            chars[r[i] - 'a']++;
-        }
-        for (int i = 0; i < m.length; i++) {
-            chars[m[i] - 'a']--;
-        }
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] > 0) {
-                return false;
-            }
+        int bucket[] = new int[26];
+        for (char c : magazine.toCharArray()) bucket[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) {
+            if (--bucket[c - 'a'] < 0) return false;
         }
         return true;
     }
