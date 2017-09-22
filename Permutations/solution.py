56459c21e8384047afb0ebe4b34a151de94b9640
dpronin <dpronin@yandex.ru>
2017-09-22 15:02:23 -0700

    Create solution.py

---
+++ Permutations/solution.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/permutations
+
+Given a collection of distinct numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.helper(nums, 0, result, len(nums))
+        return result
+
+    def helper(self, nums, current, result, max_current):
+        if current == max_current:
+            result.append(nums[:])
+            return
+        for i in range(current, max_current):
+            nums[i], nums[current] = nums[current], nums[i]
+            self.helper(nums, current + 1, result, max_current)
+            nums[i], nums[current] = nums[current], nums[i]
