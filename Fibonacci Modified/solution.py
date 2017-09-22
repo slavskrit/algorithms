ae00f3c2de3dc3538c1510429ce8c64bfec85eec
dpronin <dpronin@yandex.ru>
2017-09-22 14:08:03 -0700

    Update solution.py

--- Fibonacci Modified/solution.py
+++ Fibonacci Modified/solution.py
@@ -5,11 +5,7 @@ This is kinda stupid problem and not related to DP but for handling big integer.
 
 """
 
-t1, t2, n = map(int, input().split()) 
-
-while n != 1:
-    temp = t1 + t2 * t2
-    t1 = t2
-    t2 = temp
-    n -= 1
+t1, t2, n = map(int, input().split())
+for i in range(n - 1):
+    t2, t1 = t1 + t2 * t2, t2
 print(t1)
