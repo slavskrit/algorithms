d8b0f990e04bc84f194d2bd52e16e2b90a104411
dpronin <dpronin@yandex.ru>
2017-10-05 15:10:21 -0700

    Update Solution.java

--- Minimum Path Sum/Solution.java
+++ Minimum Path Sum/Solution.java
@@ -8,14 +8,14 @@ public class Solution {
         int x = grid[0].length;
         if (y == 0) return 0;
         int[] dp = new int[x];
-        dp[0] = grid[0][0];
-        for (int i = 1; i < x; i++) {
-            dp[i] = grid[0][i] + dp[i - 1];
-        }
-        for (int i = 1; i < y; i++) {
+        for (int i = 0; i < y; i++) {
             dp[0] += grid[i][0];
             for (int j = 1; j < x; j++) {
-                dp[j] = Math.min(dp[j - 1] + grid[i][j], dp[j] + grid[i][j]);
+                if (i == 0) {
+                    dp[j] = grid[i][j] + dp[j - 1];
+                } else {
+                    dp[j] = Math.min(dp[j - 1] + grid[i][j], dp[j] + grid[i][j]);
+                }
             }
         }
         return dp[x - 1];
