578e38a4f5117deffd8e42310337b8ab04d8cb93
dpronin <dpronin@yandex.ru>
2017-10-05 13:23:00 -0700

    Update Solution.java

--- Maximum Subarray/Solution.java
+++ Maximum Subarray/Solution.java
@@ -13,21 +13,15 @@
 // 
 public class Solution {
     public int maxSubArray(int[] nums) {
-        int length = nums.length;
-        if (length == 0) return 0;
+        if (nums.length == 0) return 0;
         int result = Integer.MIN_VALUE;
         int helper = Integer.MIN_VALUE;
         int temp = 0;
         for (int i : nums) {
             helper = Math.max(helper, i);
             result = Math.max(result, temp);
-            if (temp + i < 0) {
-                temp = 0;
-            } else {
-                temp += i;
-            }
+            temp = temp + i < 0 ? 0 : temp + i;
         }
-        int max = Math.max(result, temp);
-        return max == 0 ? helper : Math.max(result, temp);
+        return Math.max(result, temp) == 0 ? helper : Math.max(result, temp);
     }
 }
