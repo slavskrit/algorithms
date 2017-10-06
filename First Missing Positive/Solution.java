44b65a60177a1c6d6f9b7e5a9dfa38be58e08474
dpronin <dpronin@yandex.ru>
2017-10-05 21:15:03 -0700

    Update Solution.java

--- First Missing Positive/Solution.java
+++ First Missing Positive/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public int firstMissingPositive(int[] nums) {
         int result = 1;
         for (int i = 0, length = nums.length, temp = 0; i < length; i++) {
-            while (nums[i] < length && nums[i] > 0 && nums[i] - 1 != i && nums[i] != nums[nums[i] - 1]) {
+            while (nums[i] < length && nums[i] > 0 && nums[i] != nums[nums[i] - 1]) {
                 temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
