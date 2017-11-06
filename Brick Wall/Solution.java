0705320555aac3a2487ea24d06c396f957228ef2
dpronin <dpronin@yandex.ru>
2017-11-06 14:04:38 -0800

    Create Solution.java

---
+++ Brick Wall/Solution.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/brick-wall
+// 
+// There is a brick wall in front of you. The wall is rectangular and has several rows of bricks. The bricks have the same height but different width. You want to draw a vertical line from the top to the bottom and cross the least bricks.
+// 
+// The brick wall is represented by a list of rows. Each row is a list of integers representing the width of each brick in this row from left to right.
+// 
+// If your line go through the edge of a brick, then the brick is not considered as crossed. You need to find out how to draw the line to cross the least bricks and return the number of crossed bricks.
+// 
+// You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+// 
+// Example:
+// Input: 
+// [[1,2,2,1],
+//  [3,1,2],
+//  [1,3,2],
+//  [2,4],
+//  [3,1,2],
+//  [1,3,1,1]]
+// Output: 2
+// Explanation: 
+// 
+// Note:
+// The width sum of bricks in different rows are the same and won't exceed INT_MAX.
+// The number of bricks in each row is in range [1,10,000]. The height of wall is in range [1,10,000]. Total number of bricks of the wall won't exceed 20,000.
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        boolean isFirstRow = true;
+        for (List<Integer> row : wall) {
+            int currentLength = 0;
+            for (int length : row) {
+                currentLength += length;
+                int tempResult = map.getOrDefault(currentLength, 0) + 1;
+                map.put(currentLength, tempResult);
+                if (!isFirstRow && currentLength != maxLength) result = Math.max(tempResult, result);
+                isFirstRow = false;
+            }
+            if (maxLength == 0) {
+                maxLength = currentLength;
+            }
+        }
+        if (maxLength == 1) return wall.size();
+        return wall.size() - result;
+    }
+}
