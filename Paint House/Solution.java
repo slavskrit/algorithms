41efd644d3553e9f15b994d2d646be14eec66e2f
dpronin <dpronin@yandex.ru>
2017-12-15 17:33:20 -0800

    Update Solution.java

--- Paint House/Solution.java
+++ Paint House/Solution.java
@@ -11,9 +11,9 @@ class Solution {
         if (costs == null || costs.length == 0) return 0;
         int n = costs.length - 1;
         for (int i = 1; i <= n; i++) {
-            costs[i][0] += Math.min(costs[i - 1][0], costs[i - 1][1]);
-            costs[i][1] += Math.min(costs[i - 1][1], costs[i - 1][2]);
-            costs[i][2] += Math.min(costs[i - 1][2], costs[i - 1][0]);
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][2], costs[i - 1][0]);
+            costs[i][2] += Math.min(costs[i - 1][1], costs[i - 1][0]);
         }
         return Math.min(costs[n][0], Math.min(costs[n][1], costs[n][2]));
     }
