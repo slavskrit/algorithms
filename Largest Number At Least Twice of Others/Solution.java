861b7d04543e959a39c7be2c2c440e5a48d0da91
dpronin <dpronin@yandex.ru>
2018-01-03 13:26:31 -0800

    Create Solution.java

---
+++ Largest Number At Least Twice of Others/Solution.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/largest-number-at-least-twice-of-others
+// 
+// In a given integer array nums, there is always exactly one largest element.
+// 
+// Find whether the largest element in the array is at least twice as much as every other number in the array.
+// 
+// If it is, return the index of the largest element, otherwise return -1.
+// 
+// Example 1:
+// Input: nums = [3, 6, 1, 0]
+// Output: 1
+// Explanation: 6 is the largest integer, and for every other number in the array x,
+// 6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+// Example 2:
+// Input: nums = [1, 2, 3, 4]
+// Output: -1
+// Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+// Note:
+// nums will have a length in the range [1, 50].
+// Every nums[i] will be an integer in the range [0, 99].
+class Solution {
+    public int dominantIndex(int[] nums) {
+        if (nums.length < 2) return 0;
+        int largest = nums[0] > nums[1] ? nums[0] : nums[1];
+        int secondLargest = nums[0] <= nums[1] ? nums[0] : nums[1];
+        int result = nums[0] > nums[1] ? 0 : 1;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                secondLargest = nums[result];
+                result = i;
+                largest = nums[i];
+            } else if (nums[i] > secondLargest) {
+                secondLargest = nums[i];
+            }
+        }
+        return largest >= secondLargest * 2 ? result : -1;
+    }
+}
