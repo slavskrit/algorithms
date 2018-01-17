9d7397afc7fb0ad073efdbe2078b3e31fa36a7eb
dpronin <dpronin@yandex.ru>
2018-01-17 14:01:14 -0800

    Update Solution.java

--- Linked List Random Node/Solution.java
+++ Linked List Random Node/Solution.java
@@ -1,4 +1,4 @@
-// 
+// https://leetcode.com/problems/linked-list-random-node
 // 
 // Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
 // 
