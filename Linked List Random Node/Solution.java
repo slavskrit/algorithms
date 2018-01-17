26012d08edc2fba3eb88cf17bf73e2250a635187
dpronin <dpronin@yandex.ru>
2018-01-17 14:00:57 -0800

    Create Solution.java

---
+++ Linked List Random Node/Solution.java
@@ -0,0 +1,58 @@
+// 
+// 
+// Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+// 
+// Follow up:
+// What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+// 
+// Example:
+// 
+// Init a singly linked list [1,2,3].
+// 
+// ListNode head = new ListNode(1);
+// head.next = new ListNode(2);
+// head.next.next = new ListNode(3);
+// Solution solution = new Solution(head);
+// 
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    ListNode head;
+
+    public Solution(ListNode head) {
+        this.head = head;
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+        ListNode cursor = head;
+        Random random = new Random();
+        int length = 0;
+        int result = head.val;
+        while (cursor != null) {
+            length++;
+            if (random.nextInt(length) == 0) {
+                result = cursor.val;
+            }
+            cursor = cursor.next;
+        }
+        return result;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
