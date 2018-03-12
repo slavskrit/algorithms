2d89c95283b348413de185d65b562e93cb978035
dpronin <dpronin@yandex.ru>
2018-03-12 15:55:15 -0700

    Create Solution.java

---
+++ All Paths From Source to Target/Solution.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/all-paths-from-source-to-target
+// 
+// Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+// 
+// The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+// 
+// Example:
+// Input: [[1,2], [3], [3], []] 
+// Output: [[0,1,3],[0,2,3]] 
+// Explanation: The graph looks like this:
+// 0--->1
+// |    |
+// v    v
+// 2--->3
+// There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+// Note:
+// 
+// The number of nodes in the graph will be in the range [2, 15].
+// You can print different paths in any order, but you should keep the order of nodes inside one path.
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        temp.add(0);
+        dfs(graph, result, 0, temp);
+        return result;
+    }
+    
+    private void dfs(int[][] graph, List<List<Integer>> result, int current, List<Integer> temp) {
+        if (current == graph.length - 1) {
+            result.add(new ArrayList<>(temp));
+        } else {
+            for (int i : graph[current]) {
+                temp.add(i);
+                dfs(graph, result, i, temp);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+}
