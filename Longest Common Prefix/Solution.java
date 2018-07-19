2bbfe960427b4c480db857ec2eb3b4457457a415
dpronin <dpronin@yandex.ru>
2018-07-19 12:57:49 -0700

    Update Solution.java

--- Longest Common Prefix/Solution.java
+++ Longest Common Prefix/Solution.java
@@ -17,3 +17,25 @@ public class Solution {
         return result;
     }
 }
+
+// without substring() and indexOf()
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        if (strs.length == 1 || strs[0].isEmpty()) return strs[0];
+        char[] helper = strs[0].toCharArray();
+        int pointer = helper.length;
+        for (int i = 1; i < strs.length; i++) {
+            int p = 0;
+            for (int j = 0; j < strs[i].length() && p < helper.length; j++) {
+                if (helper[p] == strs[i].charAt(p)) {
+                    p++;
+                } else {
+                    pointer = Math.min(pointer, p);
+                }
+            }
+            pointer = Math.min(pointer, strs[i].length());
+        }
+        return strs[0].substring(0, pointer);
+    }
+}
