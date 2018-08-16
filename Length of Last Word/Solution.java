6ee6e04c8c5abc7bb664ac845103b1dfdb56e37c
dpronin <dpronin@yandex.ru>
2018-08-16 12:26:59 -0700

    Update Solution.java

--- Length of Last Word/Solution.java
+++ Length of Last Word/Solution.java
@@ -10,15 +10,14 @@
 // 
 public class Solution {
     public int lengthOfLastWord(String s) {
-        int lenght = 0;
-        char[] array = s.trim().toCharArray();
-        for (int i = array.length - 1; i >= 0; i--) {
-            if (array[i] == ' ') {
-                return lenght;
-            } else {
-                lenght++;
-            }
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
         }
-         return lenght;
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+        return end - start;
     }
 }
